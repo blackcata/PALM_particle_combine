6449f8302746abeba6e2bf772d028581a23e65b0
blackcata <blackcata109@gmail.com>
2019-09-21 13:26:53 +0900

    Made basic form of utility programs

---
+++ UTIL/particle_PDF/main_PDF.f90
@@ -0,0 +1,14 @@
+!------------------------------------------------------------------------------!
+!                                                                              !
+!   PROGRAM : main_PDF.f90                                                     !
+!                                                                              !
+!   PURPOSE : To make PDF functions of each particle properties                !
+!                                                                              !
+!                                                             2019.09.21 K.Noh !
+!                                                                              !
+!------------------------------------------------------------------------------!
+
+        PROGRAM main_PDF 
+          USE particle_PDF_module
+
+        END PROGRAM 
---
+++ UTIL/particle_PDF/mod_particle_PDF.f90
@@ -0,0 +1,20 @@
+!------------------------------------------------------------------------------!
+!                                                                              !
+!   PROGRAM : mod_particle_PDF.f90                                             !
+!                                                                              !
+!   PURPOSE : Calculate particle propertie PDF(Probabillity Density Function)  !
+!                                                                              !
+!                                                             2019.09.21.K.Noh !
+!                                                                              !
+!------------------------------------------------------------------------------!
+        MODULE particle_PDF_module
+            IMPLICIT NONE
+
+            SAVE 
+
+            CONTAINS
+
+            SUBROUTINE 
+            END SUBROUTINE 
+
+        END MODULE particle_PDF_module
