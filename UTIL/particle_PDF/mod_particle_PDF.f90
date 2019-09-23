e9f2e1ae8c320460e27ef24fd8da56686274314d
blackcata <blackcata109@gmail.com>
2019-09-23 11:33:58 +0900

    Made PDF caluclating & 2D Converting Subroutine

--- UTIL/particle_PDF/mod_particle_PDF.f90
+++ UTIL/particle_PDF/mod_particle_PDF.f90
@@ -34,7 +34,7 @@
 
                 Nt         =  0
                 ind_str    =  1
-                ind_end    =  577
+                ind_end    =  10
                 ind_t      =  0 
 
                 N_par      =  90000
@@ -79,14 +79,67 @@
 
 !------------------------------------------------------------------------------!
 !                                                                              !
+!   SUBROUTINE : convert_2D_1D                                                 !
+!                                                                              !
+!   PURPOSE : Convert 2D arrays to 1D arrays                                   !
+!                                                             2019.09.23 K.Noh !
+!                                                                              !
+!------------------------------------------------------------------------------!
+            SUBROUTINE convert_2D_1D(input_2D,output_1D)
+                IMPLICIT NONE
+
+                REAL(KIND=8),DIMENSION(:,:),INTENT(IN)  ::  input_2D
+                REAL(KIND=8),DIMENSION(:),INTENT(OUT)   ::  output_1D
+
+                INTEGER :: it, N_1D, ind_str, ind_end
+                INTEGER,DIMENSION(2)  :: N_2D
+
+                N_2D  =  SHAPE(input_2D)
+                N_1D  =  SIZE(output_1D)
+                
+                DO it = 1,N_2D(1) 
+                    ind_str  =  (it-1) * N_2D(2) + 1
+                    ind_end  =  (it) * N_2D(2)
+                    output_1D(ind_str:ind_end)  =  input_2D(it,:)
+                END DO 
+
+            END SUBROUTINE convert_2D_1D
+
+!------------------------------------------------------------------------------!
+!                                                                              !
 !   SUBROUTINE : calc_PDF                                                      !
 !                                                                              !
 !   PURPOSE : Calculate PDF from input 1D arrays                               !
 !                                                             2019.09.21 K.Noh !
 !                                                                              !
 !------------------------------------------------------------------------------!
-            SUBROUTINE calc_PDF
+            SUBROUTINE calc_PDF(input,PDF,PDF_min,PDF_max,N_PDF)
                 IMPLICIT NONE
+
+                INTEGER,INTENT(IN)  :: N_PDF
+                REAL(KIND=8),INTENT(IN)  ::  PDF_min, PDF_max
+                REAL(KIND=8),DIMENSION(:),INTENT(IN)  ::   input
+                REAL(KIND=8),DIMENSION(:),INTENT(OUT)  ::  PDF
+
+                INTEGER  :: it , it_bin, N_input
+                REAL(KIND=8)  ::  dPDF, tmp_bin
+
+                dPDF     =  (PDF_max - PDF_min) / (N_PDF-1)
+                N_input  =  SIZE(input)
+
+                DO it = 1,N_input
+                    DO it_bin  =  1,N_PDF
+                        tmp_bin  =  PDF_min + dPDF * (it_bin -1)
+                        IF ( input(it) > tmp_bin - dPDF*0.5        .AND.        &
+                             input(it) < tmp_bin + dPDF*0.5) THEN 
+                            PDF(it)  =  PDF(it) + 1 
+                            EXIT
+                        END IF
+                    END DO 
+                END DO 
+
+                PDF  =  PDF / N_input 
+
             END SUBROUTINE calc_PDF
 
         END MODULE particle_PDF_module
